1935953d2767a5d39e44e86755b4a68fd9978fc4
yootaebong <qscda1234@gmail.com>
2021-03-23 18:14:06 +0900

    hooks

--- habit-tracker/src/index.js
+++ habit-tracker/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import SimpleHabit from './components/simpleHabit';
 
 ReactDOM.render(
   // 개발 모드에서는 로그가 2번씩 찍힌다.
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SimpleHabit />
   </React.StrictMode>,
   document.getElementById('root'),
 );
