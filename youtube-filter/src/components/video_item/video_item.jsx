f1429f9186c0923bb9951c21e8d1af9be7abb244
yootaebong <qscda1234@gmail.com>
2021-03-25 17:45:57 +0900

    비디오 아이템 생성

--- youtube-filter/src/components/video_item/video_item.jsx
+++ youtube-filter/src/components/video_item/video_item.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const Video_item = (props) => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = ({ video: { snippet } }) => (
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        className={styles.thumbnail}
+        src={snippet.thumbnails.medium.url}
+        alt="video thumbnail"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+      </div>
+    </div>
+  </li>
+);
 
-export default Video_item;
+export default VideoItem;
--- youtube-filter/src/components/video_list/video_list.jsx
+++ youtube-filter/src/components/video_list/video_list.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Video_item from '../video_item/video_item';
+import styles from './video_list.module.css';
 
 const Video_list = (props) => {
   return (
-    <ul>
+    <ul className={styles.videos}>
       {props.videos.map((video) => {
         return <Video_item key={video.id} video={video} />;
       })}
