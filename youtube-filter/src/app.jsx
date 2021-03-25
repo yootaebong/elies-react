194e3d3a715a9d4a7b0e37325c569d538aa8ccd1
yootaebong <qscda1234@gmail.com>
2021-03-25 19:31:23 +0900

    퍼포먼스 개선

--- youtube-filter/src/app.jsx
+++ youtube-filter/src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
@@ -8,24 +8,25 @@ function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
-  const selectVideo = (video) => {
+  const selectVideo = video => {
     setSelectedVideo(video);
   };
 
-  const search = (query) => {
-    youtube
-      .search(query) //
-      .then((videos) => {
-        setVideos(videos);
-        setSelectedVideo(null);
-      });
-  };
+  const search = useCallback(
+    query => {
+      setSelectedVideo(null);
+      youtube
+        .search(query) //
+        .then(videos => setVideos(videos));
+    },
+    [youtube]
+  );
 
   useEffect(() => {
     youtube
       .mostPopular() //
-      .then((videos) => setVideos(videos));
-  }, []);
+      .then(videos => setVideos(videos));
+  }, [youtube]);
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
