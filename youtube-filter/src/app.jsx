a1d9824ba9f82bbd132fd97cd96a523339a37e9b
yootaebong <qscda1234@gmail.com>
2021-03-25 18:14:40 +0900

    add youtube service

--- youtube-filter/src/app.jsx
+++ youtube-filter/src/app.jsx
@@ -1,32 +1,27 @@
-import { useEffect, useState } from 'react';
-import './app.css';
-import Video_list from './components/video_list/video_list';
+import React, { useEffect, useState } from 'react';
+import styles from './app.module.css';
+import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
-function App() {
-  // * 비디오 리스트
-  const [videos, setVideo] = useState([]);
-  // * 마운트 또는 업데이트가 되었을떄
-  useEffect(
-    () => {
-      let url = `https://www.googleapis.com/youtube/v3/videos`;
-      url += `?regionCode=kr`;
-      url += `&type=video`;
-      url += `&chart=mostPopular`;
-      url += `&maxResults=10`;
-      url += `&key=AIzaSyAU1gA9Ah2QPVdDF0nBvui36H3xWGWsiuo`;
-      url += `&part=snippet`;
-      fetch(url, {
-        method: 'GET',
-        redirect: 'follow',
-      })
-        .then((response) => response.json())
-        .then((result) => setVideo(result.items));
-    },
-    // * 마운트가 되었을 때에만 호출
-    [],
+function App({ youtube }) {
+  const [videos, setVideos] = useState([]);
+  const search = query => {
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
+  };
+
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
+  }, []);
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
   );
-
-  return <Video_list videos={videos} />;
 }
 
 export default App;
--- youtube-filter/src/components/video_list/video_list.jsx
+++ youtube-filter/src/components/video_list/video_list.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import Video_item from '../video_item/video_item';
+import VideoItem from '../video_item/video_item';
 import styles from './video_list.module.css';
 
-const Video_list = (props) => {
-  return (
-    <ul className={styles.videos}>
-      {props.videos.map((video) => {
-        return <Video_item key={video.id} video={video} />;
-      })}
-    </ul>
-  );
-};
+const VideoList = props => (
+  <ul className={styles.videos}>
+    {props.videos.map(video => (
+      <VideoItem key={video.id} video={video} />
+    ))}
+  </ul>
+);
 
-export default Video_list;
+export default VideoList;
