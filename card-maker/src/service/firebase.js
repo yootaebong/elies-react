55e90b84436b7ee0ced9e5dcde87876e2ff1e9f6
yootaebong <qscda1234@gmail.com>
2021-03-26 16:11:32 +0900

    프로젝트 셋업 및 파이어베이스 세팅

---
+++ card-maker/src/service/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
