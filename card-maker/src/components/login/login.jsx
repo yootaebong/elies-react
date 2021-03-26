b618b2c030daf550d2c77e8c35fabcb1b13a618a
yootaebong <qscda1234@gmail.com>
2021-03-26 17:15:21 +0900

    add login & logout fuc

--- card-maker/src/components/login/login.jsx
+++ card-maker/src/components/login/login.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({ authService }) => {
-  const onLogin = event => {
+  const history = useHistory();
+  const goToMaker = (userId) => {
+    history.push({
+      pathname: '/maker',
+      state: { id: userId },
+    });
+  };
+
+  const onLogin = (event) => {
     authService //
       .login(event.currentTarget.textContent)
-      .then(console.log);
+      .then((data) => goToMaker(data.user.uid));
   };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      user && goToMaker(user.id);
+    });
+  });
+
   return (
     <section className={styles.login}>
       <Header />
@@ -18,11 +34,6 @@ const Login = ({ authService }) => {
           <li className={styles.item}>
             <button className={styles.button} onClick={onLogin}>
               Google
-            </button>
-          </li>
-          <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin}>
-              Github
             </button>
           </li>
         </ul>
--- card-maker/src/index.js
+++ card-maker/src/index.js
@@ -5,11 +5,9 @@ import App from './app';
 import AuthService from './service/auth_service';
 
 const authService = new AuthService();
-
 ReactDOM.render(
-  //  use strict 와 같은 역할
   <React.StrictMode>
     <App authService={authService} />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
